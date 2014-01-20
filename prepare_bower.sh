c8041613ab17a845ae48e65efaf6e063a972d22e
Oto Brglez <otobrglez@gmail.com>
2014-01-20 17:37:22 +0100

    Work.

--- prepare_bower.sh
+++ prepare_bower.sh
@@ -4,10 +4,10 @@ echo "Prepare Bower"
 
 if [ -z "$CIRCLECI" ]; then
 
-  if [ -d ./vendor/assets/bower_components ]; then
+  if [ -d vendor/assets/bower_components ]; then
     echo "Exists remove..."
-    rm -rf ./vendor/assets/bower_components
+    rm -rf vendor/assets/bower_components
   fi
 fi
 
-./node_modules/bower/bin/bower install
+node_modules/bower/bin/bower install
